df99e487c9a8c8648eb5ea30530b0c0b2413d540
Yanuar T. Aditya Nugraha <yanuar.aditya@smarteye.se>
2023-10-24 01:28:00 +0200

    add user input check

--- tubes1.py
+++ tubes1.py
@@ -8,11 +8,52 @@ TARIF_TOL = {
 PINTU_TOL = ["pasteur", "pasir koja", "kopo", "moh toha", "buah batu", "cileunyi"]
 TARIF_JOURNEY = 1500
 
+
+# Fungsi untuk mengecek apakah input golongan benar atau tydac.
+def is_golongan(golongan):
+    if golongan in ["1", "2", "3", "4", "5"]:
+        return True
+    else:
+        return False
+
+
+# Fungsi untuk mengecek pintu tol.
+def is_gerbang_valid(gerbang):
+    if gerbang in PINTU_TOL:
+        return True
+    else:
+        return False
+
+
 print("================== PROGRAM PINTU TOL =================")
 
-golongan = input("Masukkan golongan kendaraan (1/2/3/4/5): ")
-masuk = input("Masukkan pintu masuk tol: ")
-keluar = input("Masukkan pintu keluar tol: ")
+# input golongan.
+ask_golongan = True
+while ask_golongan:
+    golongan = input("Masukkan golongan kendaraan (1/2/3/4/5): ")
+    if is_golongan(golongan):
+        ask_golongan = False
+    else:
+        print("---WARNING: Masukkan golongan yang benar (1/2/3/4/5)")
+
+ask_gerbang = True
+while ask_gerbang:
+    masuk = input("Masukkan pintu masuk tol: ")
+    if is_gerbang_valid(masuk):
+        ask_gerbang = False
+    else:
+        print("---WARNING: Masukkan gerbang tol yang benar:")
+        print(f"---{PINTU_TOL}")
+
+ask_gerbang = True
+while ask_gerbang:
+    keluar = input("Masukkan pintu keluar tol: ")
+    if is_gerbang_valid(keluar):
+        ask_gerbang = False
+    else:
+        print("---WARNING: Masukkan gerbang tol yang benar:")
+        print(f"---{PINTU_TOL}")
+
 
 for i in range(len(PINTU_TOL)):
     print(f"sekarang loop-{i} ada di {PINTU_TOL[i]}")
